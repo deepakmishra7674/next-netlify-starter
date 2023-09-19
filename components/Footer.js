0ae6ee501685d6dfbae2c2976491e3f2e67f24fb
Deepak Mishra <deepak.mishra7674@gmail.com>
2023-09-19 14:52:59 +0400

    added mobile number into Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-//         <img src="/ogo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+         /*<img src="/ogo-netlify.svg" alt="Netlify Logo" className={styles.logo} />*/ 
+      📱:- +971 50 402 5856 
       </footer>
     </>
   )
